5be6bc5dba6b2e4960f04e1dd2b3921c895f7164
Chikage0o0 <89348590+Chikage0o0@users.noreply.github.com>
2022-10-18 13:54:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,16 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
+sed -i '$a src-git mosdns https://github.com/sbwml/luci-app-mosdns' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+./scripts/feeds install -a -f -p kenzo
+./scripts/feeds install -a -f -p small
+
+./scripts/feeds uninstall luci-app-mosdns mosdns v2ray-geodata
+./scripts/feeds install -f -p mosdns mosdns luci-app-mosdns
+find ./ -name v2ray-geodata | xargs rm -rf
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+./scripts/feeds update -i && ./scripts/feeds install -a
