6a37d30313998020efb69012f75c36ec53ec8284
Chikage0o0 <89348590+Chikage0o0@users.noreply.github.com>
2022-11-30 19:21:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,13 +25,11 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds install -a -f -p kenzo
 ./scripts/feeds install -a -f -p small
 
-./scripts/feeds uninstall luci-app-mosdns mosdns v2ray-geodata luci-app-argon-config luci-theme-argon
+./scripts/feeds uninstall luci-app-mosdns mosdns v2ray-geodata
 ./scripts/feeds install -f -p mosdns mosdns luci-app-mosdns
 find ./ -name v2ray-geodata | xargs rm -rf
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 svn export https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-vlmcsd package/luci-app-vlmcsd
 svn export https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/vlmcsd  package/vlmcsd
-svn export https://github.com/sbwml/luci-theme-argon/trunk/luci-theme-argon  package/luci-theme-argon
-svn export https://github.com/sbwml/luci-theme-argon/trunk/luci-app-argon-config  package/luci-app-argon-config
 sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' ./package/*/Makefile
 ./scripts/feeds update -i && ./scripts/feeds install -a
